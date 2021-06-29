fd2827b1b8ac5af4c427b44c99fc1326dd2be621
Olga Lomakina <olga.kraykina904@gmail.com>
2021-06-29 23:16:32 +0100

    Update Lesson 8

--- lesson8/colors.rb
+++ lesson8/colors.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
+COLORS = {
+  1 => 'red',
+  2 => 'orange',
+  3 => 'yellow',
+  4 => 'green',
+  5 => 'blue',
+  6 => 'indigo',
+  7 => 'violet'
+}.freeze
+
 def colors(number)
-  h = { 1 => 'red', 2 => 'orange', 3 => 'yellow', 4 => 'green', 5 => 'blue', 6 => 'indigo', 7 => 'violet' }
-  h[number]
+  COLORS[number]
 end
 
 puts colors(rand(1..10))
