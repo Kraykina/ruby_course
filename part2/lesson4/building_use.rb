07cb60ae3b079ad21cafa5d1ff7b059393b7a500
Olga Lomakina <olga.kraykina904@gmail.com>
2021-08-06 15:32:47 +0100

    Lesson 4

---
+++ part2/lesson4/building_use.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'lib/building'
+
+building = Building.new
+puts building.floors
+puts building.apartments
---
+++ part2/lesson4/lib/building.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Building
+  FLOORS = 19
+  APARTMENTS = 76
+
+  def floors
+    FLOORS
+  end
+
+  def apartments
+    APARTMENTS
+  end
+end
